6561a65ece85d4d3f77536f136e1dcf907028403
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-10-28 20:29:25 +0200

    minimp3 no success

--- firmware/Src/main.c
+++ firmware/Src/main.c
@@ -6,41 +6,41 @@
   ******************************************************************************
   * This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether
+  * USER CODE END. Other portions of this file, whether 
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
-  * Copyright (c) 2018 STMicroelectronics International N.V.
+  * Copyright (c) 2018 STMicroelectronics International N.V. 
   * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without
+  * Redistribution and use in source and binary forms, with or without 
   * modification, are permitted, provided that the following conditions are met:
   *
-  * 1. Redistribution of source code must retain the above copyright notice,
+  * 1. Redistribution of source code must retain the above copyright notice, 
   *    this list of conditions and the following disclaimer.
   * 2. Redistributions in binary form must reproduce the above copyright notice,
   *    this list of conditions and the following disclaimer in the documentation
   *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other
-  *    contributors to this software may be used to endorse or promote products
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
   *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this
+  * 4. This software, including modifications and/or derivative works of this 
   *    software, must execute solely and exclusively on microcontroller or
   *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under
-  *    this license is void and will automatically terminate your rights under
-  *    this license.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
   *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
   * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
   * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
   * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
   * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
   * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
@@ -145,7 +145,7 @@ int main(void)
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 512);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
@@ -155,11 +155,11 @@ int main(void)
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
-
+ 
 
   /* Start scheduler */
   osKernelStart();
-
+  
   /* We should never get here as control is now taken by the scheduler */
 
   /* Infinite loop */
@@ -187,13 +187,13 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
 
-    /**Configure the main internal regulator output voltage
+    /**Configure the main internal regulator output voltage 
     */
   __HAL_RCC_PWR_CLK_ENABLE();
 
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /**Initializes the CPU, AHB and APB busses clocks
+    /**Initializes the CPU, AHB and APB busses clocks 
     */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
@@ -208,7 +208,7 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Initializes the CPU, AHB and APB busses clocks
+    /**Initializes the CPU, AHB and APB busses clocks 
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -230,11 +230,11 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure the Systick interrupt time
+    /**Configure the Systick interrupt time 
     */
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick
+    /**Configure the Systick 
     */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -313,10 +313,10 @@ static void MX_UART4_Init(void)
 
 }
 
-/**
+/** 
   * Enable DMA controller clock
   */
-static void MX_DMA_Init(void)
+static void MX_DMA_Init(void) 
 {
   /* DMA controller clock enable */
   __HAL_RCC_DMA1_CLK_ENABLE();
@@ -328,9 +328,9 @@ static void MX_DMA_Init(void)
 
 }
 
-/** Configure pins as
-        * Analog
-        * Input
+/** Configure pins as 
+        * Analog 
+        * Input 
         * Output
         * EVENT_OUT
         * EXTI
@@ -429,7 +429,7 @@ void StartDefaultTask(void const * argument)
   audio_set_volume(50);
 
   osThreadTerminate(NULL);
-  /* USER CODE END 5 */
+  /* USER CODE END 5 */ 
 }
 
 /**
@@ -478,7 +478,7 @@ void _Error_Handler(char *file, int line)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{
+{ 
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
--- firmware/app/audio/audio.c
+++ firmware/app/audio/audio.c
@@ -4,7 +4,6 @@
 #include "audio/audio.h"
 #include "cs43l22.h"
 #include "rng/rng.h"
-#include "pub/mp3dec.h"
 
 static AudioConfig config;
 static osThreadId thread_handle;
@@ -14,13 +13,6 @@ static void thread_task(void const * args);
 static bool codec_write_register(uint8_t reg, uint8_t data);
 static uint8_t codec_read_register(uint8_t reg);
 static void codec_reset();
-
-//static uint16_t buf[2048] = { 0 }; // TODO remove
-
-MP3FrameInfo mp3FrameInfo;
-HMP3Decoder hMP3Decoder;
-extern const char mp3_data[];
-#define MP3_SIZE	687348
 
 bool audio_init(AudioConfig cfg) {
   config = cfg;
@@ -48,17 +40,6 @@ bool audio_init(AudioConfig cfg) {
   osThreadDef(thread_task, thread_task, osPriorityNormal, 0, 128);
   thread_handle = osThreadCreate(osThread(thread_task), NULL);
 
-  // TODO remove
-  /*for (size_t i = 0 ; i < sizeof(buf) / 2; i++) {
-    if (i % 2 != 0) {
-      buf[i] = 0;
-    } else {
-      buf[i] = rng_get();
-    }
-  }*/
-  //audio_play(buf, sizeof(buf) / 2);
-  //
-
   return true;
 }
 
@@ -71,76 +52,12 @@ void audio_transfer_complete_callback() {
 }
 
 // private
-static int16_t audio_buffer0[4096];
-//static int16_t audio_buffer1[4096];
+
 static void thread_task(void const * args) {
-  hMP3Decoder = MP3InitDecoder();
-  bool num = false;
+
   while(true) {
     osSemaphoreWait(sema, osWaitForever);
     //audio_play(buf, sizeof(buf) / 2); // TODO remove
-
-  	int offset, err;
-  	int outOfData = 0;
-  	static const char *read_ptr = mp3_data;
-  	static int bytes_left = MP3_SIZE;
-
-  	int16_t *samples;
-
-  	if (num || true) {
-  		samples = audio_buffer0;
-  	//	GPIO_SetBits(GPIOD, GPIO_Pin_13);
-  	//	GPIO_ResetBits(GPIOD, GPIO_Pin_14);
-  	} else {
-  	//	samples = audio_buffer1;
-  	//	GPIO_SetBits(GPIOD, GPIO_Pin_14);
-  	//	GPIO_ResetBits(GPIOD, GPIO_Pin_13);
-  	}
-
-  	offset = MP3FindSyncWord((unsigned char*)read_ptr, bytes_left);
-  	bytes_left -= offset;
-
-  	if (bytes_left <= 10000) {
-  		read_ptr = mp3_data;
-  		bytes_left = MP3_SIZE;
-  		offset = MP3FindSyncWord((unsigned char*)read_ptr, bytes_left);
-  	}
-
-  	read_ptr += offset;
-  	err = MP3Decode(hMP3Decoder, (unsigned char**)&read_ptr, &bytes_left, samples, 0);
-
-  	if (err) {
-  		/* error occurred */
-  		switch (err) {
-  		case ERR_MP3_INDATA_UNDERFLOW:
-  			outOfData = 1;
-        printf("ERR_MP3_INDATA_UNDERFLOW\n");
-  			break;
-  		case ERR_MP3_MAINDATA_UNDERFLOW:
-  			/* do nothing - next call to decode will provide more mainData */
-        printf("ERR_MP3_MAINDATA_UNDERFLOW\n");
-  			break;
-  		case ERR_MP3_FREE_BITRATE_SYNC:
-  		default:
-  			outOfData = 1;
-        printf("ERR_MP3_FREE_BITRATE_SYNC || default\n");
-  			break;
-  		}
-  	} else {
-  		/* no error */
-      printf("no err\n");
-  		MP3GetLastFrameInfo(hMP3Decoder, &mp3FrameInfo);
-  	}
-
-  	if (!outOfData) {
-      printf("ooo %u\n", mp3FrameInfo.outputSamps);
-      for(int i = 0; i < 100; i++) {
-        printf("0x%X\n", samples[i]);
-      }
-  		audio_play(samples, mp3FrameInfo.outputSamps);
-  	}
-    num = !num;
-
   }
 }
 
