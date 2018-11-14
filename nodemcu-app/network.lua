ca554b290df9a9943ce7dcf390dac0364168c29f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-11-14 06:28:18 +0200

    ntp multiple servers

--- nodemcu-app/network.lua
+++ nodemcu-app/network.lua
@@ -1,13 +1,14 @@
 
+function ntp_callback(sec, usec, server, info)
+  print('sync', sec, usec, server)
+end
+
+NTP_SERVERS = { '0.debian.pool.ntp.org', '1.debian.pool.ntp.org', '2.debian.pool.ntp.org', '3.debian.pool.ntp.org',
+  '0.opensuse.pool.ntp.org', '1.opensuse.pool.ntp.org', '2.opensuse.pool.ntp.org', '3.opensuse.pool.ntp.org',
+  '0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org', '3.pool.ntp.org', 'time.windows.com', 'pool.ntp.org',
+  '0.ru.pool.ntp.org', '1.ru.pool.ntp.org', '2.ru.pool.ntp.org', '3.ru.pool.ntp.org' }
 function ntp_sync()
-  sntp.sync("time.windows.com",
-  function(sec, usec, server, info)
-    print('sync', sec, usec, server)
-  end,
-  function()
-   print('failed!')
-  end
-)
+  sntp.sync(NTP_SERVERS, ntp_callback, nil, true)
 end
 
 function ap_connected(i)
