c30e05a6b79d11c8ec590cab9287244ee6e8036f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-11-19 09:38:24 +0200

    wifi working

--- firmware/main/wifi/wifi.c
+++ firmware/main/wifi/wifi.c
@@ -4,14 +4,23 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
-#include "rom/ets_sys.h"
+#include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event_loop.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
 
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define STA_SSID      "flhome"
+#define STA_PASS      "password_changed"
+#define MAX_CONN       5
+#define AP_SSID       "rmpd_player"
+#define AP_PASS       "12345678"
 
 /* FreeRTOS event group to signal when we are connected*/
-static EventGroupHandle_t wifi_event_group;
+//static EventGroupHandle_t wifi_event_group;
 
 /* The event group allows multiple bits for each event,
    but we only care about one event - are we connected
@@ -20,7 +29,8 @@ const int WIFI_CONNECTED_BIT = BIT0;
 
 static const char *TAG = "wifi";
 
-static esp_err_t event_handler(void *ctx, system_event_t *event) {
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
     switch(event->event_id) {
     case SYSTEM_EVENT_STA_START:
         esp_wifi_connect();
@@ -33,12 +43,9 @@ static esp_err_t event_handler(void *ctx, system_event_t *event) {
         ESP_LOGI(TAG, "station:"MACSTR" join, AID=%d", MAC2STR(event->event_info.sta_connected.mac), event->event_info.sta_connected.aid);
         break;
     case SYSTEM_EVENT_AP_STADISCONNECTED:
-        ESP_LOGI(TAG, "station:"MACSTR" leave, AID=%d", MAC2STR(event->event_info.sta_disconnected.mac), event->event_info.sta_disconnected.aid);
+        ESP_LOGI(TAG, "station:"MACSTR"leave, AID=%d", MAC2STR(event->event_info.sta_disconnected.mac), event->event_info.sta_disconnected.aid);
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
-        ESP_LOGI(TAG, "sta disconnected");
-        //ESP_ERROR_CHECK(esp_wifi_stop());
-        //ESP_ERROR_CHECK(esp_wifi_start());
         esp_wifi_connect();
         //xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
         break;
@@ -59,46 +66,48 @@ static void dhcp_server_init() {
   ESP_ERROR_CHECK(tcpip_adapter_dhcps_start(TCPIP_ADAPTER_IF_AP));
 }
 
+static void softap() {
+    wifi_config_t wifi_config = {
+      .ap = {
+        .ssid = AP_SSID,
+        .ssid_len = strlen(AP_SSID),
+        .password = AP_PASS,
+        .max_connection = MAX_CONN,
+        .authmode = WIFI_AUTH_WPA_WPA2_PSK
+      },
+    };
+    if (strlen(AP_PASS) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+
+    ESP_LOGI(TAG, "softap SSID:%s password:%s", STA_SSID, STA_PASS);
+}
+
+static void sta() {
+  wifi_config_t wifi_config = {
+    .sta = {
+      .ssid = STA_SSID,
+      .password = STA_PASS
+    },
+  };
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_LOGI(TAG, "connect to ap SSID:%s password:%s", STA_SSID, STA_PASS);
+}
+
 bool wifi_init() {
-    //wifi_event_group = xEventGroupCreate();
+  //wifi_event_group = xEventGroupCreate();
+  tcpip_adapter_init();
+  dhcp_server_init();
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
 
-    esp_wifi_set_storage(WIFI_STORAGE_RAM);
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA) );
 
-    tcpip_adapter_init();
+  softap();
+  sta();
+  ESP_ERROR_CHECK(esp_wifi_start());
 
-    dhcp_server_init();
-
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    wifi_config_t sta_config;
-
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA)); //
-
-    strcpy((char *)sta_config.sta.ssid, "flhome");
-    strcpy((char *)sta_config.sta.password, "password_changed");
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_config));
-
-    wifi_config_t ap_config;
-    strcpy((char *)ap_config.ap.ssid, "rmpd_player");
-    ap_config.ap.ssid_len = strlen("rmpd_player");
-    strcpy((char *)ap_config.ap.password, "12345678");
-    ap_config.ap.max_connection = 5;
-    ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config));
-
-
-    //if (strlen( "12345678") == 0) {
-    //    wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    //}
-
-
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-  //  xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_BIT, false, true, portMAX_DELAY);
-
-    //ESP_LOGI(TAG, "wifi init finished");
-
-    return true;
+  return true;
 }
