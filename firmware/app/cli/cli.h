1b4786ba8e1a887f3df5277aca06994c7af143e7
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-11-19 06:57:43 +0200

    esp32 hello world

--- firmware/app/cli/cli.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CLI_H
-#define __CLI_H
-
-#include "stm32f1xx_hal.h"
-
-bool cli_init(UART_HandleTypeDef *uart);
-void cli_received_callback();
-
-#endif
--- firmware/app/cli/commands.h
+++
@@ -1,6 +0,0 @@
-#ifndef __COMMANDS_H
-#define __COMMANDS_H
-
-void cli_commands_init();
-
-#endif
