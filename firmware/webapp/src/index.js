f095c98d2c707ef30b1aba11d284d361ce9708a1
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-11-24 16:59:19 +0200

    cleanup dependencies

--- firmware/webapp/src/index.js
+++ firmware/webapp/src/index.js
@@ -1,15 +1,15 @@
-import { app } from 'hyperapp';
+import { app } from 'hyperapp'
 
-import actions from './actions';
-import state from './state';
-import view from './components/Layout';
+import actions from './actions'
+import state from './state'
+import view from './components/Layout'
 
 const appArgs = [
   state,
   actions,
   view,
   document.getElementById('app'),
-];
+]
 
 function onMount(main) {
   //const { add, sub, } = main;
@@ -29,14 +29,5 @@ function onMount(main) {
 
 }
 
-let main;
-
-if (process.env.NODE_ENV !== 'production') {
-  import('hyperapp-redux-devtools').then((devtools) => {
-    main = devtools(app)(...appArgs);
-    onMount(main);
-  });
-} else {
-  main = app(...appArgs);
-  onMount(main);
-}
+let main = app(...appArgs)
+onMount(main)
