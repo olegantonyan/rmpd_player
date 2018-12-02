c91c5b83a94ee6cd9d4e1f383c62df3059a06fe6
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-12-02 10:05:13 +0200

    spi command r/w works

--- firmware/main/audio/vs1011.c
+++ firmware/main/audio/vs1011.c
@@ -9,6 +9,7 @@
 #include "soc/gpio_struct.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "esp_heap_caps.h"
 
 static const char *TAG = "vs1011";
 
@@ -21,7 +22,6 @@ static uint16_t read_sci(uint8_t addr);
 static bool dreq();
 
 bool vs1011_init() {
-  return true;
   ESP_LOGI(TAG, "initialize vs1011");
 
   gpio_set_direction(21, GPIO_MODE_OUTPUT);
@@ -32,49 +32,54 @@ bool vs1011_init() {
   reset();
 
   spi_bus_config_t bus_cfg = {
-    .miso_io_num=19,
-    .mosi_io_num=23,
-    .sclk_io_num=18,
-    .quadwp_io_num=-1,
-    .quadhd_io_num=-1,
-    .max_transfer_sz=32
+    .miso_io_num = 19,
+    .mosi_io_num = 23,
+    .sclk_io_num = 18,
+    .quadwp_io_num = -1,
+    .quadhd_io_num = -1,
+    .max_transfer_sz = 32
   };
 
   spi_device_interface_config_t data_cfg = {
-    .command_bits = 8,
-    .address_bits = 8,
-    .clock_speed_hz=4000,
-    .mode=0,
-    .spics_io_num=17,
-    .queue_size=7,
+    .command_bits = 0,
+    .address_bits = 0,
+    .clock_speed_hz = 6000000,
+    .mode = 0,
+    .spics_io_num = 17,
+    .queue_size = 7,
   };
 
   spi_device_interface_config_t command_cfg = {
-    .flags = SPI_TRANS_USE_TXDATA | SPI_TRANS_USE_RXDATA,
     .command_bits = 8,
     .address_bits = 8,
-    .clock_speed_hz=4000,
-    .mode=0,
-    .spics_io_num=7,
-    .queue_size=1,
+    .clock_speed_hz = 2000000,
+    .mode = 0,
+    .spics_io_num = 5,
+    .queue_size = 1,
   };
 
   ESP_LOGI(TAG, "initialize spi bus");
 
   ESP_ERROR_CHECK(spi_bus_initialize(VSPI_HOST, &bus_cfg, 1));
-  //ESP_LOGI(TAG, "done initialize spi bus");
-  //ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &data_cfg, &data_spi));
-  //ESP_LOGI(TAG, "done add spi device 1");
+  ESP_LOGI(TAG, "done initialize spi bus");
+  ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &data_cfg, &data_spi));
+  ESP_LOGI(TAG, "done add spi device 1");
   ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &command_cfg, &command_spi));
-  //ESP_LOGI(TAG, "done add spi device 2");
+  ESP_LOGI(TAG, "done add spi device 2");
 
-  read_sci(SCI_MODE);
+  uint16_t sci_mode = read_sci(SCI_MODE);
+  ESP_LOGI(TAG, "done read SCI_MODE %u", sci_mode);
   write_sci(SCI_MODE, SM_SDINEW|SM_SDISHARE|SM_TESTS|SM_RESET);
+  ESP_LOGI(TAG, "done write SCI_MODE");
 
   write_sci(SCI_AICTRL1, 0xABAD);
+  ESP_LOGI(TAG, "done write SCI_AICTRL1");
   write_sci(SCI_AICTRL2, 0x7E57);
-  if (read_sci(SCI_AICTRL1) != 0xABAD || read_sci(SCI_AICTRL2) != 0x7E57) {
-    ESP_LOGI(TAG, "oops :( vs1011");
+  ESP_LOGI(TAG, "done write SCI_AICTRL2");
+  uint16_t one = read_sci(SCI_AICTRL1);
+  uint16_t two = read_sci(SCI_AICTRL2);
+  if (one != 0xABAD || two  != 0x7E57) {
+    ESP_LOGI(TAG, "oops :( vs1011, 0x%X 0x%X", one, two);
   }
   ESP_LOGI(TAG, "done vs1011");
   return true;
@@ -89,24 +94,36 @@ static void reset() {
 static void write_sci(uint8_t addr, uint16_t data) {
   spi_transaction_t t;
   memset(&t, 0, sizeof(t));
+  t.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA;
   t.cmd = 2;
   t.addr = addr;
-  t.length = sizeof(data);
+  t.length = sizeof(data) * 8;
+  //uint8_t * buf = (uint8_t *)heap_caps_malloc(2, MALLOC_CAP_DMA);
+  //buf[0] = data >> 8;
+  //buf[1] = data & 0xFF;
+  //t.tx_buffer = buf;
   t.tx_data[0] = data >> 8;
   t.tx_data[1] = data & 0xFF;
-  while(!dreq());
+  while(!dreq()) {
+    taskYIELD();
+  }
   ESP_ERROR_CHECK(spi_device_polling_transmit(command_spi, &t));
 }
 
 static uint16_t read_sci(uint8_t addr) {
   spi_transaction_t t;
   memset(&t, 0, sizeof(t));
+  t.flags = SPI_TRANS_USE_RXDATA | SPI_TRANS_USE_TXDATA;
   t.cmd = 3;
   t.addr = addr;
-  t.length = sizeof(uint16_t);
-  while(!dreq());
+  t.length = sizeof(uint16_t) * 8;
+  while(!dreq()) {
+    taskYIELD();
+  }
   ESP_ERROR_CHECK(spi_device_polling_transmit(command_spi, &t));
-  return t.rx_data[0] << 8 | t.tx_data[1];
+
+  uint16_t result = (t.rx_data[0] << 8) | t.rx_data[1];
+  return result;
 }
 
 static bool dreq() {
