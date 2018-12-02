3edb18b02330bb5da1ddef9a324e644acce83b39
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-12-02 13:13:49 +0200

    vs1011 spi refactor

--- firmware/main/audio/vs1011.c
+++ firmware/main/audio/vs1011.c
@@ -9,9 +9,24 @@
 #include "soc/gpio_struct.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include "esp_heap_caps.h"
 
 static const char *TAG = "vs1011";
+
+#define VS_SO_GPIO 19
+#define VS_SI_GPIO 23
+#define VS_SCLK_GPIO 18
+#define VS_XRESET_GPIO 21
+#define VS_DREQ_GPIO 22
+#define VS_XCS_GPIO 5
+#define VS_XDCS_GPIO 17
+#define VS_XTAL_FREQ 12000000 // 12288000
+
+/* Note: code SS_VER=2 is used for both VS1002 and VS1011e */
+static const uint16_t CHIP_NUMBER[16] = {
+  1001, 1011, 1011, 1003, 1053, 1033, 1063, 1103,
+  0, 0, 0, 0, 0, 0, 0, 0
+};
+
 
 static spi_device_handle_t data_spi;
 static spi_device_handle_t command_spi;
@@ -22,73 +37,94 @@ static uint16_t read_sci(uint8_t addr);
 static bool dreq();
 
 bool vs1011_init() {
-  ESP_LOGI(TAG, "initialize vs1011");
-
-  gpio_set_direction(21, GPIO_MODE_OUTPUT);
-  gpio_set_direction(22, GPIO_MODE_INPUT);
-
-  ESP_LOGI(TAG, "reset vs1011");
+  gpio_set_direction(VS_XRESET_GPIO, GPIO_MODE_OUTPUT);
+  gpio_set_direction(VS_DREQ_GPIO, GPIO_MODE_INPUT);
 
   reset();
 
   spi_bus_config_t bus_cfg = {
-    .miso_io_num = 19,
-    .mosi_io_num = 23,
-    .sclk_io_num = 18,
+    .miso_io_num = VS_SO_GPIO,
+    .mosi_io_num = VS_SI_GPIO,
+    .sclk_io_num = VS_SCLK_GPIO,
     .quadwp_io_num = -1,
     .quadhd_io_num = -1,
     .max_transfer_sz = 32
   };
+  ESP_ERROR_CHECK(spi_bus_initialize(VSPI_HOST, &bus_cfg, 1));
 
   spi_device_interface_config_t data_cfg = {
     .command_bits = 0,
     .address_bits = 0,
     .clock_speed_hz = 6000000,
     .mode = 0,
-    .spics_io_num = 17,
+    .spics_io_num = VS_XDCS_GPIO,
     .queue_size = 7,
   };
+  ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &data_cfg, &data_spi));
 
   spi_device_interface_config_t command_cfg = {
     .command_bits = 8,
     .address_bits = 8,
-    .clock_speed_hz = 2000000,
+    .clock_speed_hz = 1000000,
     .mode = 0,
-    .spics_io_num = 5,
+    .spics_io_num = VS_XCS_GPIO,
     .queue_size = 1,
   };
-
-  ESP_LOGI(TAG, "initialize spi bus");
-
-  ESP_ERROR_CHECK(spi_bus_initialize(VSPI_HOST, &bus_cfg, 1));
-  ESP_LOGI(TAG, "done initialize spi bus");
-  ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &data_cfg, &data_spi));
-  ESP_LOGI(TAG, "done add spi device 1");
   ESP_ERROR_CHECK(spi_bus_add_device(VSPI_HOST, &command_cfg, &command_spi));
-  ESP_LOGI(TAG, "done add spi device 2");
 
-  uint16_t sci_mode = read_sci(SCI_MODE);
-  ESP_LOGI(TAG, "done read SCI_MODE %u", sci_mode);
-  write_sci(SCI_MODE, SM_SDINEW|SM_SDISHARE|SM_TESTS|SM_RESET);
-  ESP_LOGI(TAG, "done write SCI_MODE");
+  /* Start initialization with a dummy read, which makes sure our
+     microcontoller chips selects and everything are where they
+     are supposed to be and that VS10xx's SCI bus is in a known state. */
+  read_sci(SCI_MODE);
 
+  /* First real operation is a software reset. After the software
+     reset we know what the status of the IC is. You need, depending
+     on your application, either set or not set SM_SDISHARE. See the
+     Datasheet for details. */
+  write_sci(SCI_MODE, SM_SDINEW | SM_SDISHARE | SM_TESTS | SM_RESET);
+
+  /* A quick sanity check: write to two registers, then test if we
+   get the same results. Note that if you use a too high SPI
+   speed, the MSB is the most likely to fail when read again. */
   write_sci(SCI_AICTRL1, 0xABAD);
-  ESP_LOGI(TAG, "done write SCI_AICTRL1");
   write_sci(SCI_AICTRL2, 0x7E57);
-  ESP_LOGI(TAG, "done write SCI_AICTRL2");
-  uint16_t one = read_sci(SCI_AICTRL1);
-  uint16_t two = read_sci(SCI_AICTRL2);
-  if (one != 0xABAD || two  != 0x7E57) {
-    ESP_LOGI(TAG, "oops :( vs1011, 0x%X 0x%X", one, two);
+  if (read_sci(SCI_AICTRL1) != 0xABAD || read_sci(SCI_AICTRL2) != 0x7E57) {
+    ESP_LOGE(TAG, "VS1011 SPI communication failure");
+    return false;
   }
-  ESP_LOGI(TAG, "done vs1011");
+  write_sci(SCI_AICTRL1, 0);
+  write_sci(SCI_AICTRL2, 0);
+
+  /* Check VS10xx type */
+  uint16_t chip_id = (read_sci(SCI_STATUS) >> 4) & 0x0F;
+  if (CHIP_NUMBER[chip_id]) {
+    ESP_LOGI(TAG, "chip version: %d", CHIP_NUMBER[chip_id]);
+    if (CHIP_NUMBER[chip_id] != 1011) {
+      ESP_LOGE(TAG, "incorrect chip");
+      return false;
+    }
+  } else {
+    ESP_LOGE(TAG, "unknown VS10xx SCI_MODE field SS_VER = %d", chip_id);
+    return false;
+  }
+
+  /* Set the clock. Until this point we need to run SPI slow so that
+   we do not exceed the maximum speeds mentioned in
+   Chapter SPI Timing Diagram in the Datasheet. */
+  write_sci(SCI_CLOCKF, HZ_TO_SCI_CLOCKF(VS_XTAL_FREQ));
+
+  /* Set volume level at -6 dB of maximum */
+  write_sci(SCI_VOL, 0x0C0C);
+
+  ESP_LOGI(TAG, "initialization sequence completed");
+
   return true;
 }
 
 static void reset() {
-  gpio_set_level(21, 0);
-  vTaskDelay(10);
-  gpio_set_level(21, 1);
+  gpio_set_level(VS_XRESET_GPIO, 0);
+  vTaskDelay(100 / portTICK_PERIOD_MS);
+  gpio_set_level(VS_XRESET_GPIO, 1);
 }
 
 static void write_sci(uint8_t addr, uint16_t data) {
@@ -127,5 +163,5 @@ static uint16_t read_sci(uint8_t addr) {
 }
 
 static bool dreq() {
-  return gpio_get_level(22) == 1;
+  return gpio_get_level(VS_DREQ_GPIO) == 1;
 }
