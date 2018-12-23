7a451a66bdab9f5b89f10abf95526b3d43b40394
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-12-23 09:53:46 +0200

    percen pos slider

--- firmware/webapp/src/components/Home.js
+++ firmware/webapp/src/components/Home.js
@@ -3,10 +3,16 @@ import { h } from 'hyperapp'
 export default ({ state, actions }) =>
   <div id="home">
     <div class="header">
-      <h1>Player #1234</h1>
+      <h1></h1>
     </div>
 
     <div class="content">
-      <h2>{state.status.now_playing} ({state.status.percent_pos}%)</h2>
+      <h2>{state.status.now_playing}</h2>
+      <input disabled type="range" min="0" max="100" value={state.status.percent_pos} class="slider" id="percent-position" />
+
+      <div class="slidecontainer">
+        <label for="volume-control">Volume</label>
+        <input type="range" min="1" max="100" value="100" class="slider" id="volume-control" />
+      </div>
     </div>
   </div>
