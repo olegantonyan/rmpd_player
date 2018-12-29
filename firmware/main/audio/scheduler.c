40a5cf8617d817c45cc62d4bf65afc3bd752d285
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2018-12-29 17:43:52 +0200

    scheduler count number of medifiles with callback

--- firmware/main/audio/scheduler.c
+++ firmware/main/audio/scheduler.c
@@ -2,6 +2,8 @@
 #include "audio/player.h"
 
 #include <string.h>
+#include <stdio.h>
+#include <errno.h>
 #include <sys/unistd.h>
 #include <sys/stat.h>
 #include "freertos/FreeRTOS.h"
@@ -17,10 +19,13 @@
 
 static const char *TAG = "scheduler";
 
+static uint32_t total_mediafiles = 0;
+
 static void scheduler_thread(void * args);
-static void recurse_dir(const char *path, uint8_t depth);
+static void recurse_dir(const char *path, uint8_t depth, void (*callback)(const char *fullname));
 static void play(const char *path);
 static void stop();
+static void on_media_file_found(const char *path);
 
 bool scheduler_init() {
   if (!player_init()) {
@@ -51,20 +56,25 @@ static void scheduler_thread(void * args) {
     return;
   }
   closedir(dp);
+
+  total_mediafiles = 0;
+  recurse_dir(STORAGE_SD_MOUNTPOINT, 0, on_media_file_found);
+  ESP_LOGI(TAG, "total media files: %u", total_mediafiles);
+
   while(true) {
-    recurse_dir(STORAGE_SD_MOUNTPOINT, 0);
+    recurse_dir(STORAGE_SD_MOUNTPOINT, 0, play);
     taskYIELD();
   }
 }
 
-static void recurse_dir(const char *path, uint8_t depth) {
-  if (depth > 20) {
+static void recurse_dir(const char *path, uint8_t depth, void (*callback)(const char *fullname)) {
+  if (depth > 10) {
     ESP_LOGW(TAG, "directory recurse max depth reached");
     return;
   }
   DIR *dp = opendir(path);
   if (dp == NULL) {
-    ESP_LOGE(TAG, "error opening directory %s", path);
+    ESP_LOGE(TAG, "error opening directory %s: %s", path, strerror(errno));
   } else {
     while(true) {
       struct dirent *ep = readdir(dp);
@@ -78,14 +88,15 @@ static void recurse_dir(const char *path, uint8_t depth) {
         size_t newpath_len = strlen(path) + strlen(ep->d_name) + 10;
         char *newpath = malloc(newpath_len);
         snprintf(newpath, newpath_len, "%s/%s", path, ep->d_name);
-        recurse_dir(newpath, depth + 1);
+        recurse_dir(newpath, depth + 1, callback);
         free(newpath);
       } else if (string_ends_with(ep->d_name, ".mp3")) {
         size_t name_len = strlen(path) + strlen(ep->d_name) + 10;
         char *fullname = malloc(name_len);
         snprintf(fullname, name_len, "%s/%s", path, ep->d_name);
-        ESP_LOGD(TAG, "starting '%s'", fullname);
-        play(fullname);
+        if (callback != NULL) {
+          callback(fullname);
+        }
         free(fullname);
       } else {
         ESP_LOGD(TAG, "'%s' is not mp3", ep->d_name);
@@ -96,10 +107,17 @@ static void recurse_dir(const char *path, uint8_t depth) {
   }
 }
 
+static void on_media_file_found(const char *path) {
+  (void)path;
+  total_mediafiles++;
+}
+
 static void play(const char *path) {
+  ESP_LOGD(TAG, "starting '%s'", path);
   player_start(path, false);
 }
 
 static void stop() {
+  ESP_LOGD(TAG, "stopping");
   player_stop();
 }
