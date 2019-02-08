0f704512c965586f4b2e7d4124628eda3207393f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2019-02-08 23:48:17 +0200

    no more http util: need a better approach to this shit

--- firmware/main/audio/stream_playlist.c
+++ firmware/main/audio/stream_playlist.c
@@ -4,12 +4,13 @@
 #include "esp_system.h"
 #include "esp_log.h"
 #include "util/strings.h"
-#include "util/http.h"
 #include <errno.h>
+#include "esp_http_client.h"
 
 static const char *TAG = "stream_playlist";
 
 static bool fetch_playlist_by_url(const char *url, uint8_t *buffer, size_t buffer_size);
+static int http_get(const char *uri, uint8_t *data, size_t data_max_length, size_t *content_length);
 
 bool stream_playlist_parse_file(const char *fname, char *result, size_t result_length) {
   bool ok = false;
@@ -117,4 +118,30 @@ bool stream_playlist_is_stream(const char *fname) {
   }
   fclose(f);
   return result;
+}
+
+static int http_get(const char *uri, uint8_t *data, size_t data_max_length, size_t *content_length) {
+  esp_http_client_config_t config = {
+    .url = uri,
+    .timeout_ms = 3000
+  };
+  esp_http_client_handle_t client = esp_http_client_init(&config);
+  esp_err_t err = esp_http_client_perform(client);
+
+  int status = 0;
+  if (err == ESP_OK) {
+    if (esp_http_client_is_chunked_response(client)) {
+      ESP_LOGE(TAG, "chunked response is not sipported");
+    } else {
+      int bytes = esp_http_client_read(client, (char *)data, data_max_length);
+      *content_length = esp_http_client_get_content_length(client);
+      status = esp_http_client_get_status_code(client);
+      ESP_LOGD(TAG, "non-chunked, status:%d content_length:%d bytes_read:%d", status, *content_length, bytes);
+    }
+  } else {
+    ESP_LOGW(TAG, "http get request failed: %s", esp_err_to_name(err));
+  }
+  esp_http_client_cleanup(client);
+
+  return status;
 }
--- firmware/main/remote/gate.c
+++ firmware/main/remote/gate.c
@@ -1,21 +1,21 @@
 #include "remote/gate.h"
-#include "util/http.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "wifi/wifi.h"
 #include "esp_system.h"
 #include "esp_log.h"
+#include <string.h>
 
 static const char *TAG = "remote_gate";
 
 static void thread(void *_args);
 
 bool gate_init() {
-  /*BaseType_t task_created = xTaskCreate(thread, "remote_gate", 8192, NULL, 5, NULL);
+  BaseType_t task_created = xTaskCreate(thread, "remote_gate", 8192, NULL, 5, NULL);
   if (pdPASS != task_created) {
     ESP_LOGE(TAG, "cannot create thread");
     return false;
-  }*/
+  }
   return true;
 }
 
@@ -24,11 +24,6 @@ static void thread(void *_args) {
   vTaskDelay(pdMS_TO_TICKS(541));
 
   while (true) {
-    static uint8_t buf[8192] = { 0 };
-    size_t len = 0;
-
-    int s = http_get("https://server.slon-ds.ru/", buf, sizeof(buf), &len);
-    printf("status:%d bytes:%d\n%s\n", s, len, buf);
 
     vTaskDelay(pdMS_TO_TICKS(2000));
   }
