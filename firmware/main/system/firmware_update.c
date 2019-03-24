0242bf489523ad30c9abe759e048c5679c198516
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2019-03-24 18:10:40 +0200

    implement own https ota

--- firmware/main/system/firmware_update.c
+++ firmware/main/system/firmware_update.c
@@ -1,6 +1,5 @@
 #include "system/firmware_update.h"
 #include "esp_http_client.h"
-#include "esp_https_ota.h"
 #include "esp_ota_ops.h"
 #include "esp_system.h"
 #include "esp_log.h"
@@ -8,27 +7,29 @@
 #include "remote/commands/outgoing.h"
 #include "storage/nvs.h"
 #include "config/config.h"
+#include "system/sysinfo.h"
 
 static const char *TAG = "firmware_update";
 
+typedef struct {
+  bool ok;
+  esp_ota_handle_t handle;
+} OTA_Context_t;
+
 static void remove_nvs_sequence_callback(bool ok);
+static esp_err_t http_event_handle(esp_http_client_event_t *evt);
+static bool ota_start(const char *url);
 
 bool firmware_update_start(const char *url, uint32_t sequence) {
-  esp_http_client_config_t config = {
-    .url = url,
-    .cert_pem = certs(),
-    .max_redirection_count = 4,
-    .disable_auto_redirect = false,
-  };
-  esp_err_t result = esp_https_ota(&config);
+  bool result = ota_start(url);
 
-  if (result == ESP_OK) {
+  if (result) {
     ESP_LOGI(TAG, "successful upgrade, restarting, sequence %u", sequence);
     nvs_save_uint32("fw_upgdare_seq", sequence);
     config_save_disable_tls_certs_verification(false);
     esp_restart();
   } else {
-    ESP_LOGE(TAG, "upgrade failed: %s", esp_err_to_name(result));
+    ESP_LOGE(TAG, "upgrade failed");
   }
   return result == ESP_OK;
 }
@@ -67,4 +68,119 @@ static void remove_nvs_sequence_callback(bool ok) {
   }
   ESP_LOGI(TAG, "upgrade acked");
   nvs_save_uint32("fw_upgdare_seq", 0);
+}
+
+static bool ota_start(const char *url) {
+  bool ok = false;
+
+  esp_err_t err = ESP_OK;
+
+  esp_http_client_handle_t client = NULL;
+
+  do {
+    ESP_LOGI(TAG, "starting ota");
+
+    const esp_partition_t *update_partition = esp_ota_get_next_update_partition(NULL);
+    if (update_partition == NULL) {
+      ESP_LOGE(TAG, "passive ota partition not found");
+      break;
+    }
+    ESP_LOGI(TAG, "writing to partition subtype %d at offset 0x%x", update_partition->subtype, update_partition->address);
+
+    OTA_Context_t ctx;
+    ctx.ok = true;
+    ctx.handle = 0;
+
+    if (esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &ctx.handle) != ESP_OK) {
+      ESP_LOGE(TAG, "esp_ota_begin failed: %s", esp_err_to_name(err));
+      break;
+    }
+    ESP_LOGI(TAG, "esp_ota_begin succeeded");
+    ESP_LOGI(TAG, "please wait...");
+
+    esp_http_client_config_t config = {
+      .event_handler = http_event_handle,
+      .url = url,
+      .timeout_ms = 6000,
+      .method = HTTP_METHOD_GET,
+      .cert_pem = certs(),
+      .max_redirection_count = 4,
+      .disable_auto_redirect = false,
+      .user_data = (void *)&ctx,
+      .buffer_size = 2048
+    };
+    ESP_LOGI(TAG, "request to %s", config.url);
+    client = esp_http_client_init(&config);
+
+    char ua[128] = { 0 };
+    sysinfo_useragent(ua, sizeof(ua));
+    esp_http_client_set_header(client, "User-Agent", ua);
+
+    err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+      int status = esp_http_client_get_status_code(client);
+      if (200 <= status && status < 300 && ctx.ok) {
+        err = esp_ota_end(ctx.handle);
+        if (err != ESP_OK) {
+          ESP_LOGE(TAG, "esp_ota_end failed: %s", esp_err_to_name(err));
+          break;
+        }
+
+        err = esp_ota_set_boot_partition(update_partition);
+        if (err != ESP_OK) {
+          ESP_LOGE(TAG, "esp_ota_set_boot_partition failed: %s", esp_err_to_name(err));
+          break;
+        }
+        ESP_LOGI(TAG, "esp_ota_set_boot_partition succeeded");
+        ok = true;
+      }
+    } else {
+      ESP_LOGE(TAG, "http get request failed: %s", esp_err_to_name(err));
+      break;
+    }
+
+  } while(false);
+
+  esp_http_client_cleanup(client);
+
+  return ok;
+}
+
+static esp_err_t http_event_handle(esp_http_client_event_t *evt) {
+  switch(evt->event_id) {
+    case HTTP_EVENT_ERROR:
+      ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+      break;
+    case HTTP_EVENT_ON_CONNECTED:
+      ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+      break;
+    case HTTP_EVENT_HEADER_SENT:
+      ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+      break;
+    case HTTP_EVENT_ON_HEADER:
+      ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER %s = %s", evt->header_key, evt->header_value);
+      break;
+    case HTTP_EVENT_ON_DATA:
+      ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+
+      int status = esp_http_client_get_status_code(evt->client);
+      if (300 <= status && status < 599) {
+        break;
+      }
+
+      OTA_Context_t *ctx = (OTA_Context_t *)evt->user_data;
+      esp_err_t ota_write_err = esp_ota_write(ctx->handle, evt->data, evt->data_len);
+      if (ota_write_err != ESP_OK) {
+        ESP_LOGE(TAG, "OTA write error: %s", esp_err_to_name(ota_write_err));
+        ctx->ok = false;
+      }
+      break;
+    case HTTP_EVENT_ON_FINISH:
+      ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+      break;
+    case HTTP_EVENT_DISCONNECTED:
+      ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+      break;
+  }
+  return ESP_OK;
 }
